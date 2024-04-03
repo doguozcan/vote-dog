ad09bfe21832d2d914051807fe7b0ed9550757f3
doguozcan <76822280+doguozcan@users.noreply.github.com>
2024-04-03 14:37:48 +0300

    Implement App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,21 @@
+import Dog from './features/dog/Dog'
+import LikeButton from './features/dog/LikeButton'
+import DislikeButton from './features/dog/DislikeButton'
+import Likes from './features/dog/Likes'
+import Dislikes from './features/dog/Dislikes'
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return (
+    <div className="flex flex-col">
+      <div className="flex justify-center py-5 gap-10 text-primary">
+        <Likes />
+        <Dislikes />
+      </div>
+      <Dog />
+      <div className="flex justify-center gap-2">
+        <LikeButton />
+        <DislikeButton />
+      </div>
+    </div>
+  )
 }
