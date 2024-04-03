c3acaaa8d3049f567bba0112a0ae18dae72f944c
doguozcan <76822280+doguozcan@users.noreply.github.com>
2024-04-03 13:15:01 +0300

    Provide store in the main file & add dog reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import dogReducer from '../features/dog/dogSlice'
 
 export default configureStore({
-  reducer: {},
+  reducer: {
+    dog: dogReducer,
+  },
 })
